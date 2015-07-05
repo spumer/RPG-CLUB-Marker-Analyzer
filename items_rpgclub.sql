880a73bb86d9aee2840318502505800fbced6f8e
spumer <spumer-tm@yandex.ru>
2015-07-05 14:04:48 +0500

    Add items data

---
+++ items_rpgclub.sql
@@ -0,0 +1,6 @@
+INSERT INTO items (id, name) VALUES (26164, 'Libaesun helmet');
+INSERT INTO items (id, name) VALUES (26610, 'None');
+INSERT INTO items (id, name) VALUES (26611, 'None');
+INSERT INTO items (id, name) VALUES (26612, 'None');
+INSERT INTO items (id, name) VALUES (27019, 'Chronicler''s Letter');
+INSERT INTO items (id, name) VALUES (27023, 'RUR');
